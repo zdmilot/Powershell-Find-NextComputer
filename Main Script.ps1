a35bcd70844969ca5c703526d4854118d2233725
Zachary Milot <zdmilot@gmail.com>
2015-07-07 09:37:45 -0400

    Creation

---
+++ Main Script.ps1
@@ -0,0 +1,3 @@
+﻿<# What I want to happen is for powershell to display the next computer in line at the top intro, but allow for a computer only search below using a loop to keep searching until the user closes out the session#>
+Import-Module ActiveDirectory
+Get-ADComputer -Filter * -Property * | Format-Table Name,OperatingSystem,OperatingSystemServicePack,OperatingSystemVersion -Wrap –Auto
