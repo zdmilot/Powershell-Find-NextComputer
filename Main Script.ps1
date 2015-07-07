10cfb410feef66eb0a9f67428bf61fcded377cf7
Zachary Milot <zdmilot@gmail.com>
2015-07-07 10:17:16 -0400

    added a testing file, and set up the main file for structure

--- Main Script.ps1
+++ Main Script.ps1
@@ -1,3 +1,20 @@
 ﻿<# What I want to happen is for powershell to display the next computer in line at the top intro, but allow for a computer only search below using a loop to keep searching until the user closes out the session#>
+
+
+
+
+
+
+Write-Host "**********************************************************************************`n
+            
+Powershell Find Computers in AD Script`n
+   Writen By: Zachary Milot`n
+            
+**********************************************************************************`n"
 Import-Module ActiveDirectory
 Get-ADComputer -Filter * -Property * | Format-Table Name,OperatingSystem,OperatingSystemServicePack,OperatingSystemVersion -Wrap –Auto
+:serviceloop while(1) {
+                        $Search = Read-Host "Enter Serarch"
+                        Get-ADComputer -Filter $Search -Property * | Format-Table Name,OperatingSystem,OperatingSystemServicePack,OperatingSystemVersion -Wrap –Auto
+                       
+                        }
