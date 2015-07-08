ad34a66c7029c05db85c13053a7ca6ac4c3a61e2
Zachary Milot <zdmilot@gmail.com>
2015-07-08 12:37:48 -0400

    added removing everything but numbers from computer list

--- Main Script.ps1
+++ Main Script.ps1
@@ -1,10 +1,17 @@
 ﻿<# What I want to happen is for powershell to display the next computer in line at the top intro, but allow for a computer only search below using a loop to keep searching until the user closes out the session#>
+Import-Module ActiveDirectory
+
+$r = Get-ADComputer -Filter 'Name -like "MA-LT*"' | select-object -expandproperty name #| Select-Object -first 1 #This will get the first one in a list, but it doesnt nessasarily mean that it is the last in the sequence
+$r -creplace '[^0-9]',''
+$r.TrimStart('0')
+
+ write-host $r
+
+Start-Sleep -s 1000000
 
 
 
-
-
-
+<#
 Write-Host "**********************************************************************************`n
             
 Powershell Find Computers in AD Script`n
@@ -18,3 +25,5 @@ Get-ADComputer -Filter * -Property * | Format-Table Name,OperatingSystem,Operati
                         Get-ADComputer -Filter $Search -Property * | Format-Table Name,OperatingSystem,OperatingSystemServicePack,OperatingSystemVersion -Wrap –Auto
                        
                         }
+
+                        #>
